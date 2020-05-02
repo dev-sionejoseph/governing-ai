ca128933e1518dc866ec8732cb16c9896cc660c9
Student_M9 <Student_M9@ps-dal-m9.home>
2020-05-02 14:01:04 -0500

    Sione's components

--- governing-ai-app/src/App.js
+++ governing-ai-app/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Introduction from './components/Introduction';
+import BridgingRights from './components/BridgingRights';
+
 
 function App() {
   return (
     <div className="App">
-      .
+      <Introduction />
+      <BridgingRights />
     </div>
   );
 }
