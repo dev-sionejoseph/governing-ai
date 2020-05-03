8fdf4dc19debac9cd70d0bff17b44fa26421388d
aparna4scholas <aparna4scholas@gmail.com>
2020-05-02 20:37:30 -0500

    modified path

--- governing-ai-app/src/components/Humanrights.js
+++ governing-ai-app/src/components/Humanrights.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import img2 from '../images/Pros-and-Cons-of-Artificial-Intelligence-1280x720.jpg';
-import img1 from '../images/5-Dangers-of-Artificial-Intelligence-in-the-Future.png';
+import img2 from '../assets/images/Pros-and-Cons-of-Artificial-Intelligence-1280x720.jpg';
+import img1 from '../assets/images/5-Dangers-of-Artificial-Intelligence-in-the-Future.png';
 
 
 
@@ -9,7 +9,7 @@ function Humanrights ()
     return(
 
         <div className="d1"> 
-        <h2 >  A Humanrights for AI risks and Harms</h2>
+        <h2  className="main_header">  A Humanrights for AI risks and Harms</h2>
         <div className="d2">
     
       <img className="i1" src = {img1} alt='Human rights'/>
