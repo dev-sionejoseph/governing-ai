4898b28796f2c63dd6dafdec11f9ca335886017f
aparna4scholas <aparna4scholas@gmail.com>
2020-05-05 07:25:02 -0500

    more images added

--- governing-ai-app/src/components/Humanrights.js
+++ governing-ai-app/src/components/Humanrights.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import img2 from '../assets/images/Pros-and-Cons-of-Artificial-Intelligence-1280x720.jpg';
-import img1 from '../assets/images/IWD2015Banner_Panama_UNWomen_675x313.jpg';
 
+import img1 from '../assets/images/IWD2015Banner_Panama_UNWomen_675x313.jpg';
+import img2 from '../assets/images/wef.png'
+import img3 from '../assets/images/wef-back.png'
+import img4 from '../assets/images/FreedomOfExpression.png'
+import img5 from '../assets/images/freedomofexpression-back.jpeg'
 
 
 function Humanrights() {
@@ -10,17 +13,42 @@ function Humanrights() {
     <div className="d1" >
       <h1 className="main_header">  Humanrights for AI </h1>
       <div className="flip-box">
-        <div className="flip-box-inner">
+        <div className="flip-box-image">
           <div className="flip-box-front">
             <img className="img1" src={img1} alt='Human rights' />
           </div>
           <div className="flip-box-back">
-          <p>
-            "No one born from hating another person beacuse of the color of his skin,or his background,or his religion.people must learn to hate,and if they can be taught to love,for love comes more naturally to the human heart than its possible."
+          <p className="p_un">
+            UN: "All humans being or born free & equal in dignity and rights
+            "
             <br></br>
             
-            .....Nelson mandela ...
+            .....Article 1 of the Universal declaration of Humanrights ...
           </p>
+        </div>
+      </div>
+
+     </div>
+     <div className="flip-box">
+        <div className="flip-box-image">
+          <div className="flip-box-front">
+          <img className="img2" src={img2} alt='Human rights' />
+          </div>
+          <div className="flip-box-back">
+                      <img className="img3" src={img3} alt='Human rights' />
+
+        </div>
+      </div>
+
+     </div>
+     <div className="flip-box">
+        <div className="flip-box-image">
+          <div className="flip-box-front">
+          <img className="img4" src={img4} alt='Human rights' />
+          </div>
+          <div className="flip-box-back">
+                      <img className="img5" src={img5} alt='Human rights' />
+
         </div>
       </div>
 
